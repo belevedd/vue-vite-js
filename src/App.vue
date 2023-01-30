5bb68a5746c5fb128d659a2a7787ce8242db86d2
hz <1144061645@qq.com>
2023-01-30 17:15:51 +0800

    引入loading组件

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,12 @@
     <router-link to="/login">登录</router-link>
     <router-link to="/home">首页</router-link>
     <router-view />
+    <loading />
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import loading from '@/components/loading/loading.vue'
+</script>
 
 <style scoped></style>
