895ad29a3890c1b845f1b11615b890f4afaa1812
hz <1144061645@qq.com>
2023-01-30 17:09:57 +0800

    添加loading公共组件

---
+++ src/components/loading/loading.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="loading"
+    v-if="mainStore.isLoading"
+    element-loading-text="正在请求数据..."
+    v-loading="mainStore.isLoading"
+  ></div>
+</template>
+
+<script setup>
+import useMainStore from '@/stores/modules/main'
+
+const mainStore = useMainStore()
+</script>
+
+<style lang="less" scoped>
+.loading {
+  position: fixed;
+  z-index: 999;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
