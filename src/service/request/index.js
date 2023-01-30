2ca9e879fb4e941accb8e6d70dde9d12c920871e
hz <1144061645@qq.com>
2023-01-30 17:12:51 +0800

    axios请求中添加请求/响应拦截器；并在请求成功时加载loading组件，响应成功/失败时移除loading组件

--- src/service/request/index.js
+++ src/service/request/index.js
@@ -1,13 +1,63 @@
 import axios from 'axios'
-
 import { BASE_URL, TIME_OUT } from './config'
+import useMainStore from '@/stores/modules/main'
+
+const mainStore = useMainStore()
 
 class myRequest {
-  constructor(baseURL, timeout = 10000) {
+  constructor(baseURL, timeout = 10000, showLoading = true) {
     this.instance = axios.create({
       baseURL,
       timeout,
     })
+    // 请求时是否显示loading动画
+    this.showLoading = showLoading
+
+    // 添加所有的实例都有的拦截器
+    this.instance.interceptors.request.use(
+      (config) => {
+        // 所有的实例都有的拦截器: 请求成功拦截
+        if (this.showLoading) {
+          mainStore.isLoading = true
+        }
+        return config
+      },
+      (err) => {
+        // 所有的实例都有的拦截器: 请求失败拦截
+        return err
+      }
+    )
+
+    this.instance.interceptors.response.use(
+      (res) => {
+        // 所有的实例都有的拦截器: 响应成功拦截
+
+        // 将loading移除
+        mainStore.isLoading = false
+
+        return res
+
+        /* 数据处理示例：
+        if (data.returnCode === '-1001') {
+          console.log('请求失败~, 错误信息')
+        } else {
+          return data
+        } */
+      },
+      (err) => {
+        // 所有的实例都有的拦截器: 响应失败拦截
+
+        // 将loading移除
+        mainStore.isLoading = false
+
+        /* 判断不同的HttpErrorCode显示不同的错误信息示例：
+        if (err.response.status === 404) {
+          console.log('404的错误~')
+        } */
+
+        return err
+      }
+    )
   }
 
   request(config) {
