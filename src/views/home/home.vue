359fde0b95a2cfdd74ed98ead83533ef10e758a3
hz <1144061645@qq.com>
2023-01-30 11:24:35 +0800

    更新添加home模块网络请求功能

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,9 +1,22 @@
 <template>
   <div class="home">
     <h2>home</h2>
+    <ul>
+      <template v-for="(item, index) in cbcList">
+        <li>{{ item.certificateLocation }}</li>
+      </template>
+    </ul>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { storeToRefs } from 'pinia'
+import useHomeStore from '@/stores/modules/home'
+
+const homeStore = useHomeStore()
+homeStore.fetchCbcListData()
+const { cbcList } = storeToRefs(homeStore)
+console.log(cbcList.value)
+</script>
 
 <style lang="less" scoped></style>
