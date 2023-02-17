5d1182177d0d09da6b9753432e394ba1a6e67682
hz <1144061645@qq.com>
2023-02-17 17:29:59 +0800

    添加轮播图案例

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -9,9 +9,59 @@
       <el-button type="warning">Warning</el-button>
       <el-button type="danger">Danger</el-button>
     </el-row>
+    <div class="my-swiper">
+      <swiper
+        class="my-swiper"
+        :modules="modules"
+        :slides-per-view="1"
+        :space-between="50"
+        navigation
+        :pagination="{ clickable: true }"
+        :scrollbar="{ draggable: true }"
+        @swiper="onSwiper"
+        @slideChange="onSlideChange"
+      >
+        <swiper-slide class="my-swiper-slide">Slide 1</swiper-slide>
+        <swiper-slide class="my-swiper-slide">Slide 2</swiper-slide>
+        <swiper-slide class="my-swiper-slide">Slide 3</swiper-slide>
+      </swiper>
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
 
-<style lang="less" scoped></style>
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from 'swiper/vue'
+
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/scrollbar'
+
+const onSwiper = (swiper) => {
+  console.log(swiper)
+}
+const onSlideChange = () => {
+  console.log('slide change')
+}
+const modules = [Navigation, Pagination, Scrollbar, A11y]
+</script>
+
+<style lang="less" scoped>
+.my-swiper {
+  width: 700px;
+  height: 300px;
+
+  .my-swiper-slide {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
