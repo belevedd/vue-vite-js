71e6b2d0628beb3d0e2625f4075d84bc4817f431
hz <1144061645@qq.com>
2023-03-24 10:24:58 +0800

    添加格式化当前时间方法

--- src/utils/format-date.js
+++ src/utils/format-date.js
@@ -7,3 +7,8 @@ export function formatMonthDay(date, formatStr = 'MM月DD日') {
 export function getDiffDays(startDate, endDate) {
   return dayjs(endDate).diff(startDate, 'day')
 }
+
+export function formatCurrentTime(formatStr = 'YYYY/MM/DD HH:mm:ss') {
+  const currentTime = dayjs()
+  return dayjs(currentTime).format(formatStr)
+}
