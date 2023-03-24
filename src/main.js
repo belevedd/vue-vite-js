d186ad9c51cee94e89541e819bb9dc250d318a65
hz <1144061645@qq.com>
2023-03-24 17:38:24 +0800

    引入Elmessage和Elloading的css样式文件

--- src/main.js
+++ src/main.js
@@ -7,6 +7,10 @@ import 'normalize.css'
 import './assets/css/index.css'
 import '@/assets/icon/iconfont.css'
 
+//引入Elmessage和Elloading的css样式文件
+import 'element-plus/theme-chalk/el-loading.css'
+import 'element-plus/theme-chalk/el-message.css'
+
 const app = createApp(App)
 app.use(router)
 app.use(pinia)
