283123ede7783dfda264bd9a7759df76bcec463d
hz <1144061645@qq.com>
2023-03-29 18:38:59 +0800

    添加选择框

--- src/components/form/filter-form.vue
+++ src/components/form/filter-form.vue
@@ -1,24 +1,44 @@
 <template>
   <div class="filter-form" v-if="filterData.length > 0">
-    <template v-for="(item, index) in filterData" :key="index">
+    <template v-for="item in filterData" :key="item.field">
       <div v-if="item.type === 'input'" class="item">
-        <span>{{ item.text }}</span>
+        <span :style="{ width: filterSpanWidth }">{{ item.text }}</span>
         <el-input
           style="width: 270px"
           v-model="filterForm[item.field]"
           clearable
+          :placeholder="item.placeholder"
           @change="changeForm()"
         />
       </div>
-      <div v-else-if="item.type === 'select-multiple'" class="item">
-        <span>{{ item.text }}</span>
+      <div v-else-if="item.type === 'select'" class="item">
+        <span :style="{ width: filterSpanWidth }">{{ item.text }}</span>
         <el-select
           style="width: 270px"
-          v-model="item.value"
-          multiple
+          v-model="filterForm[item.field]"
+          filterable
           collapse-tags
           collapse-tags-tooltip
-          placeholder="Select"
+          :placeholder="item.placeholder"
+        >
+          <el-option
+            v-for="(optionItem, index) in item.options"
+            :key="index"
+            :label="optionItem.label"
+            :value="optionItem.value"
+          />
+        </el-select>
+      </div>
+      <div v-else-if="item.type === 'select-multiple'" class="item">
+        <span :style="{ width: filterSpanWidth }">{{ item.text }}</span>
+        <el-select
+          style="width: 270px"
+          v-model="filterForm[item.field]"
+          multiple
+          filterable
+          collapse-tags
+          collapse-tags-tooltip
+          :placeholder="item.placeholder"
         >
           <el-option
             v-for="(optionItem, index) in item.options"
@@ -29,9 +49,9 @@
         </el-select>
       </div>
       <div v-else-if="item.type === 'date-picker'" class="item">
-        <span>{{ item.text }}</span>
+        <span :style="{ width: filterSpanWidth }">{{ item.text }}</span>
         <el-date-picker
-          style="width: 250px"
+          style="max-width: 250px"
           v-model="filterForm[item.field]"
           type="daterange"
           :range-separator="item.rangeSeparator"
@@ -56,6 +76,7 @@
   text: 'input-text',
   field: 'input-field',
   value: '',
+  placeholder: '',
 },
 ----- select-多选 -----
 {
@@ -77,6 +98,7 @@
       label: 'Option3x',
     },
   ],
+  placeholder: '',
 },
 ----- date-picker -----
 {
@@ -108,9 +130,30 @@ const { filterData } = toRefs(props)
 
 // 过滤表单数据
 const filterForm = ref({})
-filterData.value.forEach((item) => {
+// 过滤表单最长的文字
+const filterLongestText = ref('')
+const filterSpanWidth = ref('')
+// 格式化表单中文字宽度方法
+const formatFormWidth = (longestText) => {
+  // 新建一个 span
+  let span = document.createElement('span')
+  span.innerText = longestText
+  document.body.appendChild(span)
+  let width = span.getBoundingClientRect().width
+  // 移除 document 中临时的 span
+  document.body.removeChild(span)
+  return width
+}
+// 获取父组件传来的表单配置和格式化显示样式
+for (let [index, item] of filterData.value.entries()) {
   filterForm.value[item.field] = item.value
-})
+  if (item.text.length > filterLongestText.value.length) {
+    filterLongestText.value = item.text
+  }
+  if (index === filterData.value.length - 1) {
+    filterSpanWidth.value = formatFormWidth(filterLongestText.value) + 'px'
+  }
+}
 
 // 内容修改后数据发送给父组件
 const changeForm = () => {
@@ -147,6 +190,7 @@ defineExpose({
     margin: 5px 0;
 
     span {
+      display: inline-block;
       margin-right: 10px;
       white-space: nowrap;
     }
