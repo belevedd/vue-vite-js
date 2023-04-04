ac706db63593da2e20fd5a3ff9f5fcce49ace659
hz <1144061645@qq.com>
2023-04-04 13:14:31 +0800

    添加加载动态路由

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import pinia from './stores'
+import useMenuStore from '@/stores/modules/home/menu'
 
 import 'normalize.css'
 import './assets/css/index.css'
@@ -12,7 +13,11 @@ import 'element-plus/theme-chalk/el-loading.css'
 import 'element-plus/theme-chalk/el-message.css'
 
 const app = createApp(App)
-app.use(router)
 app.use(pinia)
+
+const menuStore = useMenuStore()
+menuStore.loadLocalMenu()
+
+app.use(router)
 
 app.mount('#app')
