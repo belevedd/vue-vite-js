aa9d5430a233e3eeddb611b6ae215bd42607e628
hz <1144061645@qq.com>
2023-04-04 15:00:16 +0800

    添加用户管理下载模板按钮组件

---
+++ src/views/home/system/user/cpns/user_02-5-download.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="btn-item">
+    <el-button
+      :color="commonBtnColor.btnDownloadColor"
+      @click="downloadBtnClick()"
+    >
+      <i class="icon"><i-ep-download /></i>
+      <span>下载模板</span>
+    </el-button>
+  </div>
+</template>
+
+<script setup>
+/*----- 引入配置项 -----*/
+import { commonBtnColor } from '@/config/common/style'
+
+const downloadBtnClick = () => {
+  console.log('点击下载模板按钮')
+}
+</script>
+
+<style lang="less" scoped>
+.btn-item {
+  margin: 5px 10px 0 0;
+}
+</style>
