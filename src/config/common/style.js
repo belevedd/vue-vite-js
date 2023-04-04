6168ee6ebb84403053e2033a1223f852d3dcc60b
hz <1144061645@qq.com>
2023-04-04 14:13:36 +0800

    添加公共样式配置

---
+++ src/config/common/style.js
@@ -0,0 +1,21 @@
+// 公共按钮颜色配置
+const commonBtnColor = {
+  btnPrimaryColor: '#168eea', //按钮主要颜色
+  btnQueryColor: '#168eea', //查询按钮颜色
+  btnResetColor: '#1b4c43', //重置按钮颜色
+  btnEditColor: '#fcd337', // 编辑按钮颜色
+  btnDeleteColor: '#d22d3d', //删除按钮颜色
+  btnBackColor: '#90b4cd', //返回按钮颜色
+  btnUploadColor: '#1eb66a', //导入按钮颜色
+  btnDownloadColor: '#18a099', //导入按钮颜色
+  btnViewColor: '#f3750e', //查看按钮颜色
+}
+
+// 表格头部样式配置
+const tableHeaderStyle = {
+  color: '#000000',
+  backgroundColor: '#f8f9fa',
+  lineHeight: '40px',
+}
+
+export { commonBtnColor, tableHeaderStyle }
