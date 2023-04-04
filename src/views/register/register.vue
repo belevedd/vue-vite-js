0bcb26869ecd50ba1c8f986cb541b8f216c193cb
hz <1144061645@qq.com>
2023-04-04 13:28:17 +0800

    修改注册页面背景图片

--- src/views/register/register.vue
+++ src/views/register/register.vue
@@ -40,6 +40,6 @@ const loginClick = () => {
   align-items: center;
   width: 100vw;
   height: 100vh;
-  background: url('@/assets/img/common/bg.jpg') center/cover no-repeat;
+  background: url('@/assets/img/login/bg.png') center/cover no-repeat;
 }
 </style>
