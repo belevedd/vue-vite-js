04aba09155e1ee03c15d9ac4efe604ae38321136
hz <1144061645@qq.com>
2023-04-04 14:14:42 +0800

    修改主页菜单显示内容

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,23 +1,65 @@
 <template>
   <div class="home">
-    <h2>home</h2>
-    <i class="icon-danganguanli" style="font-size: 30px; color: #f00"></i>
-    <ul>
-      <template v-for="(item, index) in cbcList" :key="index">
-        <li>{{ item.certificateLocation }}</li>
-      </template>
-    </ul>
+    <el-container>
+      <el-header class="home-header">
+        <img :src="getAssetURL('common/logo.png')" alt="logo" />
+        <h1>滑稽草莓的组件库</h1>
+      </el-header>
+      <el-container class="home-content">
+        <el-aside class="home-aside">
+          <home-menu />
+        </el-aside>
+        <el-main>
+          <router-view />
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script setup>
-import { storeToRefs } from 'pinia'
-import useHomeStore from '@/stores/modules/home'
-
-const homeStore = useHomeStore()
-homeStore.fetchCbcListData()
-const { cbcList } = storeToRefs(homeStore)
-console.log(cbcList.value)
+/*----- 引入子组件 -----*/
+import HomeMenu from './menu/menu.vue'
+/*----- 引入访问本地图片方法 -----*/
+import { getAssetURL } from '@/utils/load-assets'
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.home {
+  width: 100%;
+  height: 100%;
+  background-color: #f5f7fb;
+
+  .home-header {
+    display: flex;
+    align-items: center;
+    height: 50px;
+    background-color: #fff;
+    box-shadow: 0 0 20px rgba(25, 124, 207, 0.08);
+
+    img {
+      height: 26px;
+      margin-right: 10px;
+    }
+    h1 {
+      font-size: 26px;
+      font-weight: 500;
+      font-family: '华文行楷';
+    }
+  }
+  .home-content {
+    width: 100%;
+    height: calc(100vh - 60px);
+    margin-top: 10px;
+
+    .home-aside {
+      width: auto;
+      border-top: 1px solid #e6edef;
+      border-right: 1px solid #e6edef;
+      background-color: #fff;
+      box-shadow: 0 0 20px rgba(25, 124, 207, 0.08);
+      box-sizing: border-box;
+    }
+  }
+}
+</style>
