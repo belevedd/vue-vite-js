2e8af35469e9bed8bcd1434315a8f318e13a92db
hz <1144061645@qq.com>
2023-04-04 13:29:26 +0800

    添加导航守卫

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import localCache from '@/utils/cache'
+import { firstMenu } from '@/utils/menu/map-menus'
 
 const router = createRouter({
   history: createWebHashHistory(),
@@ -10,17 +12,34 @@ const router = createRouter({
     },
     {
       path: '/login',
+      name: 'login',
       component: () => import('@/views/login/login.vue'),
     },
     {
       path: '/register',
+      name: 'register',
       component: () => import('@/views/register/register.vue'),
     },
     {
       path: '/home',
+      name: 'home',
       component: () => import('@/views/home/home.vue'),
     },
   ],
+})
+
+// -导航守卫
+router.beforeEach((to) => {
+  if (to.path !== '/login') {
+    const userMenus = localCache.getCache('userMenus')
+    if (!userMenus) {
+      return '/login'
+    }
+  }
+
+  if (to.path === '/home') {
+    return firstMenu.url
+  }
 })
 
 export default router
