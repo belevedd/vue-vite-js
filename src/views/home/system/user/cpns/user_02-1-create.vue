81a77408cbef773669f3c1d25365155c0753e18f
hz <1144061645@qq.com>
2023-04-04 14:34:06 +0800

    添加用户管理新建用户按钮组件

---
+++ src/views/home/system/user/cpns/user_02-1-create.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="btn-item">
+    <el-button
+      :color="commonBtnColor.btnPrimaryColor"
+      @click="createBtnClick()"
+    >
+      <i class="icon"><i-ep-document-add /></i>
+      <span>新建</span>
+    </el-button>
+  </div>
+</template>
+
+<script setup>
+/*----- 引入配置项 -----*/
+import { commonBtnColor } from '@/config/common/style'
+
+const createBtnClick = () => {
+  console.log('点击新建按钮')
+}
+</script>
+
+<style lang="less" scoped>
+.btn-item {
+  margin: 5px 10px 0 0;
+}
+</style>
